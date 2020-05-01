657e590bcf10cf1a5573c03398bc36ca29c5c86b
François Grondin <Francois.Grondin2@USherbrooke.ca>
2020-05-01 16:15:09 -0400

    Add files via upload

---
+++ python/dataset/audio.py
@@ -0,0 +1,82 @@
+from torch.utils.data import Dataset
+import json
+import librosa as lr
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Audio(Dataset):
+
+	def __init__(self, file_meta):
+
+		with open(file_meta) as f:
+			self.elements = f.read().splitlines()
+
+	def __len__(self):
+
+		return len(self.elements)
+
+	def __getitem__(self, idx):
+
+		audio = json.loads(self.elements[idx])
+
+		fs = 16000
+
+		mics = audio['farfield']['mics']
+		srcs = audio['farfield']['srcs']
+		speed = audio['farfield']['speed']
+		snrs = audio['farfield']['snr']
+		gains = audio['farfield']['gain']
+		volume = audio['farfield']['volume']
+		path = audio['farfield']['path']
+
+		hs, _ = lr.core.load(path=path, sr=16000, mono=False)
+
+		nSrcs = len(srcs)
+		nMics = len(mics)
+
+		duration = audio['speech'][0]['duration']
+		N = round(duration * fs)
+
+		xs = np.zeros((nSrcs,nMics,N), dtype=np.float32)
+
+		for iSrc in range(0, nSrcs):
+
+			path = audio['speech'][iSrc]['path']
+			offset = audio['speech'][iSrc]['offset']
+
+			s, _ = lr.core.load(path=path, sr=fs, mono=True, offset=offset, duration=duration)
+
+			snr = snrs[iSrc]
+
+			for iMic in range(0, nMics):
+
+				gain = gains[iMic]
+
+				index = iSrc * nMics + iMic
+				h = hs[index,:]
+
+				x = np.convolve(h,s,mode='same')
+
+				x /= np.sqrt(np.mean(x**2))
+				x *= 10.0 ** (snr/20.0)
+				x *= gain
+
+				xs[iSrc,iMic,range(0,x.shape[0])] = x
+
+		xs /= np.max(xs)
+		xs *= volume
+
+		tdoas = np.zeros((nSrcs, nMics), dtype=np.float32)
+
+		for iSrc in range(0, nSrcs):
+
+			src = np.array(srcs[iSrc])
+			src -= np.mean(np.array(mics), axis=0)
+			src /= np.sqrt(np.sum(src**2))
+
+			for iMic in range(0, nMics):
+
+				mic = mics[iMic] - np.mean(np.array(mics), axis=0)
+				tdoas[iSrc,iMic] = (fs/speed) * np.dot(mic, src)
+
+		return xs, tdoas
---
+++ python/dataset/pair.py
@@ -0,0 +1,88 @@
+
+from torch.utils.data import Dataset
+from dataset.audio import Audio
+
+import json
+import librosa as lr
+import numpy as np
+
+class Pair(Dataset):
+
+	def __init__(self, file_meta, file_json):
+
+		self.audio = Audio(file_meta=file_meta)
+
+		with open(file_json, 'r') as f:
+			features = json.load(f)
+
+		self.frameSize = features['frame_size']
+		self.hopSize = features['hop_size']
+		self.c = features['c']
+		self.alpha = features['alpha']
+		self.beta = features['beta']
+		self.epsilon = features['epsilon']
+
+	def __len__(self):
+
+		return len(self.audio)
+
+	def __getitem__(self, idx):
+
+		xs, tdoas = self.audio[idx]
+
+		nSrcs = xs.shape[0]
+		nMics = 2
+
+		y1 = 0.001 * np.random.randn(xs.shape[2])
+		y2 = 0.001 * np.random.randn(xs.shape[2])
+		T1 = 0.0
+		T2 = 0.0
+		I1 = np.abs(lr.core.stft(y1, n_fft=self.frameSize, hop_length=self.hopSize))**2
+		I2 = np.abs(lr.core.stft(y2, n_fft=self.frameSize, hop_length=self.hopSize))**2
+
+		tau12ref = tdoas[0,0] - tdoas[0,1]
+
+		for iSrc in range(0, nSrcs):
+
+			tau12 = tdoas[iSrc,0] - tdoas[iSrc,1]
+
+			dtau = np.abs(tau12 - tau12ref)
+
+			gain = 1.0 - 1.0 / (1.0 + np.exp(-1.0 * self.alpha * (dtau - self.beta)))
+
+			x1 = xs[iSrc,0,:]
+			x2 = xs[iSrc,1,:]
+
+			y1 += x1
+			y2 += x2
+
+			X1 = lr.core.stft(x1, n_fft=self.frameSize, hop_length=self.hopSize)
+			X2 = lr.core.stft(x2, n_fft=self.frameSize, hop_length=self.hopSize)
+
+			T1 += np.abs(gain * X1)**2
+			T2 += np.abs(gain * X2)**2
+			I1 += np.abs(X1)**2
+			I2 += np.abs(X2)**2
+
+		Y1 = lr.core.stft(y1, n_fft=self.frameSize, hop_length=self.hopSize)
+		Y2 = lr.core.stft(y2, n_fft=self.frameSize, hop_length=self.hopSize)
+
+		M1 = T1 / (I1 + self.epsilon)
+		M2 = T2 / (I2 + self.epsilon)
+
+		Y12 = Y1 * np.conj(Y2)
+
+		k = np.expand_dims(np.arange(0, self.frameSize/2+1), axis=1)
+		f = np.ones((1, Y12.shape[1]), dtype=np.float32)
+		A = np.exp(-1j*2*np.pi*tau12ref*k*f/self.frameSize)
+
+		X12 = np.transpose(A * Y12)
+		M12 = np.transpose(M1 * M2)
+
+		X = np.zeros((X12.shape[0],X12.shape[1],2), dtype=np.float32)
+		X[:,:,0] = np.log(np.abs(X12)**2 + self.epsilon) - np.log(self.epsilon)
+		X[:,:,1] = np.angle(X12)
+
+		M = M12
+
+		return X, M
